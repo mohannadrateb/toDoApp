3fda592d9b3a72e68c5060881ed4b755d35759a5
mohannad rateb <mohannadrateb84@gmail.com>
2019-06-10 23:01:57 +0200

    corrected the dynamic content

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+
+class App extends Component {
+
+  state ={
+    todos:[
+      {id:1,content:'LEARN REACT'},
+      {id:2,content:'LEARN DEUTSCH'},
+      {id:3,content:'meet FRIENDS'},
+      {id:4,content:'TRAIN'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className = "center blue-text"> todo's</h1>
+        <Todos todos = {this.state.todos}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Todos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Todos = ({todos}) =>{
+const todoList = todos.length ? (todos.map(todo=>{
+return(<div className = "collection-item" key ={todo.id} >
+<span>todo.content</span>
+
+</div> 
+    )
+})):(
+    <p className = "Center"> YOU HAVE NO TODO'S LEFT</p>
+)
+
+return(
+    <div className = "todos collection">
+    {todoList}
+
+    </div>
+)
+
+
+
+
+
+
+}
+
+export default Todos;
